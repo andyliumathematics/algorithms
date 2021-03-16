af0375d40e22dedf953151a12ae0049c613c58dc
AndyLiu <andyliumathematics@163.com>
2021-03-16 21:11:30 +0800

    冒泡排序

--- src/main/java/com/andyliu/algorithms/popsort/PopSort.java
+++ src/main/java/com/andyliu/algorithms/popsort/PopSort.java
@@ -1,21 +1,21 @@
-package com.andyliu.algorithms.selectsort;
+package com.andyliu.algorithms.popsort;
 
-public class SelectSort {
+public class PopSort {
     public static void main(String[] args) {
-        int arr[] = { 3, 4, 2345, 6, 43, 3, 3, 2345, 77, 957, 908, 354, 6 };
+        int arr[] = {3, 4, 2345, 6, 43, 3, 3, 2345, 77, 957, 908, 354, 6};
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
-        //选择排序
-        for (int nowtop = 0; nowtop < arr.length - 1; nowtop++) {
-            int minpos = nowtop;
+        //冒泡排序
+        for (int bottompos = arr.length; bottompos >= 0; bottompos--) {
+            int minpos = bottompos;
             for (int i = minpos + 1; i < arr.length; i++) {
                 if (arr[minpos] > arr[i])
                     minpos = i;
             }
-            int tmp = arr[nowtop];
-            arr[nowtop] = arr[minpos];
+            int tmp = arr[bottompos];
+            arr[bottompos] = arr[minpos];
             arr[minpos] = tmp;
         }
         for (int i = 0; i < arr.length; i++) {
